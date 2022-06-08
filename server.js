4d4b4960f93fbca1d51e620d57fb635b850e4d07
bpc2003 <wpesfriendnva@gmail.com>
2022-06-07 22:42:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -18,10 +18,6 @@ app.use("/api/contact", contactAPI);
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static("./client/build"));
-
-  app.get('*', (req, res) =>{
-    res.sendFile(__dirname + "client/build/index.html");
-  });
 }
 
 console.log(process.env.PORT)
